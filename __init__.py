fe0767a112a45eb1d926c56d95f0b4b9deddbd76
Dmitriy Kalpakchi <dkalpackchi@gmail.com>
2016-01-08 13:14:53 +0200

    How to prevent errors

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 # Will give:
 # ValueError: Attempted relative import in non-package
 
+# To prevent this error:
+# 1. import jpg
+#    import png
+# 2. From the parent directory
+#    python -m rel_import.__init__
+
 from . import jpg
 from . import png
 
